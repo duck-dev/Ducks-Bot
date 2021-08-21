fea646718650f1c2b3009d03fb0bd6d662dad88d
Duck <61479436+NiceDuck@users.noreply.github.com>
2021-08-21 13:53:28 +0200

    Connecting the bot
    - Added logic for connecting the bot
    - Added logic for registering all commands
    - Added logic for defining a prefix (or leaving the default prefix)

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
 
 namespace Ducks_Bot
 {
@@ -6,7 +9,25 @@ namespace Ducks_Bot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args[0], (args.Length > 1 && args[1].Length > 0) ? args[1] : "!").GetAwaiter().GetResult();
         }
+
+        private static async Task MainAsync(string token, string prefix)
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.GuildMembers
+            });
+            
+            CommandsNextExtension commands = discord.UseCommandsNext(new CommandsNextConfiguration() {
+                StringPrefixes = new[] { prefix[0].ToString() } // ! will be the default command prefix if nothing else is specified in the parameters
+            });
+            commands.RegisterCommands(Assembly.GetExecutingAssembly()); // Registers all defined commands
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        } 
     }
 }
