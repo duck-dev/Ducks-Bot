c22089bbb46245d645053ff333993be33d1b13b2
Duck <61479436+NiceDuck@users.noreply.github.com>
2021-08-21 13:19:53 +0200

    First Commit
    - Added 'src' folder for the code
    - Moved 'Program.cs' into the recently created 'src' folder

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ducks_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
