5d1c0e3813e82c267d10c13596b820810fab18db
Duck <61479436+NiceDuck@users.noreply.github.com>
2021-08-23 18:50:58 +0200

    Added kicking users
    - Added the 'kick' command (Kick.cs including the partial class Moderation as 'Ban.cs' and 'Delete.cs' as well.

---
+++ src/Commands/Moderation/Kick.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace DucksBot.Commands
+{
+    public partial class Moderation
+    {
+        [Command("kick")]
+        [Description("Kicks the specified user.")]
+        [RequirePermissions(Permissions.BanMembers)]
+        [RequireRoles(RoleCheckMode.Any, "Mod")]
+        public async Task KickCommand(CommandContext ctx, 
+            [Description("User to kick.")] DiscordMember user)
+        {
+            string description = $"{user.DisplayName} has been successfully banned!";
+            await Kick(user, ctx, description);
+        }
+
+        [Command("kick")]
+        [Description("Kicks the specified user with a specified reason.")]
+        [RequirePermissions(Permissions.BanMembers)]
+        [RequireRoles(RoleCheckMode.Any, "Mod")]
+        public async Task KickCommand(CommandContext ctx, 
+            [Description("User to kick.")] DiscordMember user, 
+            [Description("Reason for the kick.")] string reason)
+        {
+            string description = $"{user.DisplayName} has been successfully kicked for the following reason:{Environment.NewLine}**{reason}**";
+            await Kick(user, ctx, description, reason);
+        }
+
+        private static async Task Kick(DiscordMember user, CommandContext ctx, string description, string reason = null)
+        {
+            if (user is null)
+            {
+                await Utilities.ErrorCallback(CommandErrors.InvalidUser, ctx);
+                return;
+            }
+            
+            try
+            {
+                await user.RemoveAsync(reason);
+                await Utilities.BuildEmbedAndExecute($"Kicked {user.DisplayName}", description, Utilities.Green, ctx, false);
+            }
+            catch (Exception e)
+            {
+                await Utilities.ErrorCallback(CommandErrors.UnknownError, ctx);
+            }
+        }
+    }
+}
