dc22459aa40df9908701af4daeb8f5bea3fb52a7
Duck <61479436+NiceDuck@users.noreply.github.com>
2021-08-29 21:02:16 +0200

    Added "Miscellaneous commands"
    - Added "MiscellaneousCommands.cs" for small, uncategorized commands. The first one that was added is the 'random' command, which returns a random integer between two specified numbers.

---
+++ src/Commands/MiscellaneousCommands.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace DucksBot.Commands
+{
+    /// <summary>
+    /// Miscellaneous commands, not specifically dedicated to a specific thing
+    /// </summary>
+    public class MiscellaneousCommands : BaseCommandModule
+    {
+        private readonly Random random = new Random();
+        
+        [Command("random")]
+        [Description("Get a random number in the specified range (both values INCLUSIVE)")]
+        public async Task RandomCommand(CommandContext ctx, 
+            [Description("Min value")] int a, 
+            [Description("Max value")] int b)
+        {
+            if (a > b)
+            {
+                await Utilities.BuildEmbedAndExecuteAsync("Error", 
+                    "The first value should be smaller than or equal to the second number", Utilities.Red, ctx.Channel);
+                return;
+            }
+            
+            int num = random.Next(a, b + 1);
+            await ctx.RespondAsync(num.ToString());
+        }
+    }
+}
